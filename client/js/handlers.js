06c4af3467247f218fd6f003540a9586672f460b
akbiggs <alex.biggs@utoronto.ca>
2013-04-20 11:19:06 -0400

    Applied getting location to 'missed satellite' button

--- client/js/handlers.js
+++ client/js/handlers.js
@@ -1,38 +1,9 @@
 var handlers = {
     sawIt: function() {
-        if (navigator.geolocation) {
-            $("#location-form").html("<p>Finding location...</p>");
-
-            var locationMarker = null;
-            navigator.geolocation.getCurrentPosition(
-                function(position) {
-                    // check if already have the location
-                    // fixes bug in FF where invoked more than once due to
-                    // cached result
-                    if (locationMarker) {
-                        return;
-                    }
-
-                    locationMarker = {
-                        "lat": position.coords.latitude,
-                        "long": position.coords.longitude
-                    };
-                    
-                    $("#location-form").html("");
-                    map.showMap(locationMarker);
-                }
-            );
-            
-        } else {
-            var form = "";
-            form += "<label for='location'>Enter your location: </label>";
-            form += "<input type='text' id='location' />";
-            form += "<button onClick='handlers.submitLocation()'></button>";
-            $("#location-form").html(form);
-        }
+        map.getLocation(map.showMap);
     },
 
     missedIt: function() {
-        console.log("I missed it...");
+        map.getLocation(map.showMap);
     },
 };
--- client/js/map.js
+++ client/js/map.js
@@ -1,10 +1,53 @@
 var map = {
+    map: $K.map("#map", 800, 400),
+    mapURL: "",
+
+    loadMap: function() {
+        this.map.loadMap(this.mapURL, function(myMap) {
+            // do something
+        });
+    },
+
     showMap: function(pos) {
+        //this.loadMap();
         $("#map").html("<p>Map of surrounding results for " 
-                + this.locString(pos) + " goes here.</p>");
+                + map.locString(pos) + " goes here.</p>");
     },
 
     locString: function(pos) {
         return "(" + pos.lat + ", " + pos.long + ")"
+    },
+
+    getLocation: function(callback) {
+        if (navigator.geolocation) {
+            $("#location-form").html("<p>Finding location...</p>");
+
+            var locationMarker = null;
+            navigator.geolocation.getCurrentPosition(
+                function(position) {
+                    // check if already have the location
+                    // fixes bug in FF where invoked more than once due to
+                    // cached result
+                    if (locationMarker) {
+                        return;
+                    }
+
+                    locationMarker = {
+                        "lat": position.coords.latitude,
+                        "long": position.coords.longitude
+                    };
+                    
+                    $("#location-form").html("");
+                    callback(locationMarker);
+                }
+            );
+            
+        } else {
+            var form = "";
+            form += "<label for='location'>Enter your location: </label>";
+            form += "<input type='text' id='location' />";
+            form += "<button onClick='handlers.submitLocation()'></button>";
+            $("#location-form").html(form);
+        }
     },
 };
