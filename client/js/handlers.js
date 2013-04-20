3af88d25901938275f0e01567081e65449c0c29e
akbiggs <alex.biggs@utoronto.ca>
2013-04-20 11:41:42 -0400

    Hiding buttons after clicked

--- client/js/handlers.js
+++ client/js/handlers.js
@@ -1,9 +1,15 @@
 var handlers = {
     sawIt: function() {
+        this.hideUI();
         map.getLocation(map.showMap);
     },
 
     missedIt: function() {
+        this.hideUI();
         map.getLocation(map.showMap);
     },
+
+    hideUI: function() {
+        $(".content-before-map").fadeOut("slow", 0);
+    }
 };
