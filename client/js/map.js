31055d48aa999f7d19a66763fe1e7d6652a891d3
NatMorcos <n.morcos@mail.utoronto.ca>
2013-04-21 11:05:53 -0400

    panzoom pos adjusted, parsing densities from server

--- client/js/map.js
+++ client/js/map.js
@@ -23,42 +23,68 @@ var pick_colour = function(num){
     }
 };
 
-var initPanZoom = function(){
-            var panZoom = mapping.map.paper.panzoom({ initialZoom:5, initialPosition: { x: 0, y: 0 } });
-            panZoom.enable()
+var initPanZoom = function(pos){
+    console.log(pos)
+    var panZoom = mapping.map.paper.panzoom({ 
+        initialZoom : 4,
+        initialPosition : {
+            x : $('#results.ui-page').width() * (180.0 + pos.long)/360,
+            y : $('#results.ui-page').height() * (90 - pos.lat)/360, 
+        }
+    });
+    console.log(panZoom.currPos);
+    panZoom.enable()
 
-            setInterval(function() {
-                $('h1').html(panZoom.currZoom +' x:'+panZoom.currPos.x.toFixed(0)+' y:'+panZoom.currPos.y.toFixed(0));
-            }, 50);
-            var css = '<style type="text/css">.grabbing { cursor: url(data:image/x-icon;base64,AAACAAEAICACAAcABQAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAAAEAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD8AAAA/AAAAfwAAAP+AAAH/gAAB/8AAAH/AAAB/wAAA/0AAANsAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gH///4B///8Af//+AD///AA///wAH//+AB///wAf//4AH//+AD///yT/////////////////////////////8=), pointer !important; } .map-controls div { cursor:pointer; font-size: 20px; color: #777; font-weight:bold; font-family: Helvetica; line-height: 28px; text-align:center;border: 1px solid #bbb; } .map-controls div:hover { border: 1px solid #999; color: #000; }</style>';
-            $('body').append(css);
+    setInterval(function() {
+        $('h1').html(panZoom.currZoom +' x:'+panZoom.currPos.x.toFixed(0)+' y:'+panZoom.currPos.y.toFixed(0));
+        }, 50);
+    var css = '<style type="text/css">.grabbing { cursor: url(data:image/x-icon;base64,AAACAAEAICACAAcABQAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAAAEAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD8AAAA/AAAAfwAAAP+AAAH/gAAB/8AAAH/AAAB/wAAA/0AAANsAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gH///4B///8Af//+AD///AA///wAH//+AB///wAf//4AH//+AD///yT/////////////////////////////8=), pointer !important; } .map-controls div { cursor:pointer; font-size: 20px; color: #777; font-weight:bold; font-family: Helvetica; line-height: 28px; text-align:center;border: 1px solid #bbb; } .map-controls div:hover { border: 1px solid #999; color: #000; }</style>';
+    $('body').append(css);
 
-            // init pan zoom controls
-            var ctrls = $('<div />'), up = $('<div>+</div>'), down = $('<div>-</div>');
-            ctrls
-                .addClass('map-controls')
-                .css({ position: 'absolute', top: 20, left: 20, 'z-index': 1000 })
-                .append(up).append(down);
-            up.css({ 'border-radius': '14px 14px 0 0', width: 28, height: 28, position: 'absolute',
-                top: 0, left: 0, background: '#fff' });
-            down.css({ 'border-radius': '0 0 14px 14px', width: 28, height: 28, position: 'absolute',
-                top: 29, left: 0, background: '#fff' })
-            $('#map').parent().append(ctrls);
-            up.click(function (e) {
-                panZoom.zoomIn(1);
-                e.preventDefault();
-            });
-            down.click(function (e) {
-                panZoom.zoomOut(1);
-                e.preventDefault();
-            });
+    // init pan zoom controls
+    var ctrls = $('<div />'), 
+        up = $('<div>+</div>'), 
+        down = $('<div>-</div>');
+
+    ctrls
+        .addClass('map-controls')
+        .css({ position: 'absolute', top: 20, right: 20, 'z-index': 1000 })
+        .append(up).append(down);
+        up.css({
+            'border-radius': '14px 14px 0 0', 
+            width: 28, 
+            height: 28, 
+            position: 'absolute',
+            top: 0, 
+            right: 0, 
+            background: '#fff' 
+        });
+        down.css({ 
+            'border-radius': '0 0 14px 14px', 
+            width: 28, 
+            height: 28, 
+            position: 'absolute',
+            top: 29, 
+            right: 0, 
+            background: '#fff' 
+        });
+        $('#map').parent().append(ctrls);
+            
+        up.click(function (e) {
+            panZoom.zoomIn(1);
+            e.preventDefault();
+        });
+        down.click(function (e) {
+            panZoom.zoomOut(1);
+            e.preventDefault();
+        });
 }
 
 var mapping = { 
     map: Kartograph.map("#map" , w),
     mapURL: "../svg/world.svg",
 
-    loadMap: function(densities, callback) {
+    loadMap: function(densities, pos, callback) {
         mapping.map.loadMap(mapping.mapURL, function() {
             mapping.map.addLayer("provinces", {
 		        
@@ -93,12 +119,12 @@ var mapping = {
                     child.setAttribute("fill", pick_colour(densities[title]));
                 });
             });
-            initPanZoom();
+            initPanZoom(pos);
         });
     },
 
     showMap: function(densityData, pos) {
-        mapping.loadMap(densityData, function(map) {
+        mapping.loadMap(densityData, pos, function(map) {
             $.mobile.changePage("#results");
             $(".ui-page").css("background-image", "none");
         });
@@ -139,7 +165,13 @@ var mapping = {
 
                     $.post("http://192.168.0.113:8000/space/add", params, function(densityData) {
                         $("#location-form").html("<p>Rendering map...</p>");
-                        callback(JSON.parse(densityData), location);
+
+                        densityData = JSON.parse(densityData);
+                        $.each( function(density){
+                            densityData[density] = parseFloat(densityData[density]);
+
+                        });
+                        callback(densityData, location);
                     });
                 }
             );
